923a53d3a7b0e0fccd456778a6cb21a924c6e701
Abhijit Gupta <abhijit.gupta@appdirect.com>
2019-11-15 13:49:30 -0500

    initial spring setup

---
+++ soswedding/src/main/java/com/penelakut/soswedding/SosweddingApplication.java
@@ -0,0 +1,13 @@
+package com.penelakut.soswedding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SosweddingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SosweddingApplication.class, args);
+	}
+
+}
---
+++ soswedding/src/test/java/com/penelakut/soswedding/SosweddingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.penelakut.soswedding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SosweddingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

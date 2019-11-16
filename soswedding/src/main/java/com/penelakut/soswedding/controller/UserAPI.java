c37ee03d5e7d7ef8f472ecc9c25a8d22efe88549
Abhijit Gupta <abhijit.gupta@appdirect.com>
2019-11-16 17:33:19 -0500

    modified user repo and class, trying to connect to mysql.

--- soswedding/src/main/java/com/penelakut/soswedding/controller/UserAPI.java
+++ soswedding/src/main/java/com/penelakut/soswedding/controller/UserAPI.java
@@ -1,4 +1,28 @@
 package com.penelakut.soswedding.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.penelakut.soswedding.model.User;
+import com.penelakut.soswedding.repository.UserRepository;
+
+@RestController
 public class UserAPI {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/user/{id}")
+    public User getUser(@PathVariable String id){
+        return userRepository.findById(Long.parseLong(id)).orElse(null);
+    }
+
+    @PostMapping("/user")
+    public User postUser(@RequestBody User user){
+        return userRepository.save(user);
+    }
 }
--- soswedding/src/main/java/com/penelakut/soswedding/model/User.java
+++ soswedding/src/main/java/com/penelakut/soswedding/model/User.java
@@ -1,4 +1,28 @@
 package com.penelakut.soswedding.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor
 public class User {
+    @Id
+    @Column(name = "id", nullable = false, unique = true)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String type;
+
 }
